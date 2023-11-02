5fa53688a7bc84cf479c1657ecdfba832e67bfe8
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-02 19:57:40 -0300

    Upadte Client

--- src/entities/Client.java
+++ src/entities/Client.java
@@ -1,8 +1,11 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Client {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	private String name;
 	private String email;
@@ -42,6 +45,8 @@ public class Client {
 		this.birthDate = birthDate;
 	}
 	
-	
+	public String toString() {
+		return name +" (" + sdf.format(birthDate) + ") - " + email;
+	}
 
 }
