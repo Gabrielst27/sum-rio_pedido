35a49c5765c33e3a55c67e14f5a71b62e933ace3
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-02 21:16:59 -0300

    Instanciados product e item em Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,9 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
 import entities.enums.OrderStatus;
 
 public class Program {
@@ -31,6 +34,25 @@ public class Program {
 		System.out.println("Enter order data:");
 		System.out.print("Status: ");
 		OrderStatus status = OrderStatus.valueOf(sc.next());
+		
+		System.out.printf("\nHow many items to this order?: ");
+		int itemsQuantity = sc.nextInt();
+		
+		for(int i = 1; i <= itemsQuantity; i++) {
+			sc.nextLine();
+			System.out.println();
+			System.out.printf("Enter item #%d data:\n", i);
+			System.out.print("Product name: ");
+			String prodName = sc.nextLine();
+			System.out.print("Product price: ");
+			double price = sc.nextDouble();
+			System.out.print("Quantity: ");
+			int quantity = sc.nextInt();
+			
+			Product product = new Product(prodName, price);
+			OrderItem item = new OrderItem(quantity, price, product);
+			
+			}
 		
 		sc.close();
 
